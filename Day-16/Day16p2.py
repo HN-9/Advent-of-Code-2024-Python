2d7cd83595aa23be5220c5fbbe8d0cbfdf1d89dd
Harsh Vardhan Singh <142162991+HN-9@users.noreply.github.com>
2025-01-01 23:12:27 +0530

    Update Day16p2.py

--- Day-16/Day16p2.py
+++ Day-16/Day16p2.py
@@ -1,4 +1,4 @@
-with open("Day-16-Challenge/input.txt", "r") as file:
+with open("input.txt", "r") as file:
     lines = file.readlines()
 final_grid = [[""]*(len(lines)) for _ in range(len(lines[0]) - 1)]
 
