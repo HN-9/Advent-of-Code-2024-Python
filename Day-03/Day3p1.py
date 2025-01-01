b1a9dba877b37d32e65ca20b4353c37293313abb
Harsh Vardhan Singh <142162991+HN-9@users.noreply.github.com>
2025-01-01 21:40:07 +0530

    Create Day3p1.py

---
+++ Day-03/Day3p1.py
@@ -0,0 +1,13 @@
+import re
+
+with open("Day-3-Challenge\input.txt", "r") as file:
+    corrupted_memory = file.read()
+
+pattern = r"mul\((\d{1,3}),(\d{1,3})\)" 
+
+matches = re.findall(pattern,corrupted_memory)
+
+result_sum = 0
+for x,y in matches:
+    result_sum += int(x) * int(y)
+print(result_sum)
