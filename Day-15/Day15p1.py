4cf41a8fead2683cfe2929c65ab6973b257f9e8c
Harsh Vardhan Singh <142162991+HN-9@users.noreply.github.com>
2025-01-01 23:13:53 +0530

    Update Day15p1.py

--- Day-15/Day15p1.py
+++ Day-15/Day15p1.py
@@ -1,4 +1,4 @@
-with open("Day-15-Challenge/input.txt", "r") as file:
+with open("input.txt", "r") as file:
     lines = file.readlines()
 final_grid = [["."]*(50) for _ in range(len(lines[0]) - 1)]
 
