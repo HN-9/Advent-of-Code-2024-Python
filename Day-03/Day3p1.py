80c115ab94f492fac3486c2fc2dd6744ae65d7f0
Harsh Vardhan Singh <142162991+HN-9@users.noreply.github.com>
2025-01-01 23:23:53 +0530

    Update Day3p1.py

--- Day-03/Day3p1.py
+++ Day-03/Day3p1.py
@@ -1,6 +1,6 @@
 import re
 
-with open("Day-3-Challenge\input.txt", "r") as file:
+with open("input.txt", "r") as file:
     corrupted_memory = file.read()
 
 pattern = r"mul\((\d{1,3}),(\d{1,3})\)" 
